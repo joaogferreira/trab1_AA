07455938f427be1d6d6677a55f21514ac0ee2aef
joaogferreira <jgmpof@ua.pt>
2020-12-01 23:00:36 +0000

    dynamic read from file

--- dynamic/dynamic_needleman_wunsch.py
+++ dynamic/dynamic_needleman_wunsch.py
@@ -1,5 +1,14 @@
+import sys
+import argparse
+import logging
+import faulthandler
+import time
 
-def score(x,y,i,j):
+parser = argparse.ArgumentParser()
+sys.setrecursionlimit(1000000)
+
+
+def score(x,y,i,j,match, mismatch):
     if(x[j]==y[i]):
         return 1 #match
     else:
@@ -17,7 +26,7 @@ def find_path(matrix, m, n):
     
 
     left = matrix[m][n-1] - 1 #pos anterior + gap
-    diagonal = matrix[m-1][n-1] + score(x,y, m-1, n-1) 
+    diagonal = matrix[m-1][n-1] + score(x,y, m-1, n-1,match,mismatch) 
     top = matrix[m-1][n] - 1  #pos anterior + gap 
 
     choice = max(left, diagonal, top)
@@ -34,7 +43,7 @@ def find_path(matrix, m, n):
         
 
 
-def alignment(x,y):
+def alignment(x,y,match,mismatch,gap):
     n = len(x)
     m = len(y)
 
@@ -42,14 +51,14 @@ def alignment(x,y):
     matrix = [ [0 for i in range(n+1)] for j in range(m+1)]
 
     for i in range(1, m+1):
-        matrix[i][0] = matrix[i-1][0] - 1 #pos anterior + gap 
+        matrix[i][0] = matrix[i-1][0] + gap #pos anterior + gap 
 
     for j in range(1, n+1):
-        matrix[0][j] = matrix[0][j-1] - 1 #pos anterior + gap
+        matrix[0][j] = matrix[0][j-1] + gap #pos anterior + gap
     
         for i in range(1,m+1):
             for j in range(1, n+1):
-                matrix[i][j] = max(matrix[i-1][j-1] + score(x,y,i-1,j-1),  matrix[i-1][j]-1, matrix[i][j-1]-1) #diagonal (align), top (delete), left (insert)
+                matrix[i][j] = max(matrix[i-1][j-1] + score(x,y,i-1,j-1,match,mismatch),  matrix[i-1][j]-1, matrix[i][j-1]-1)
 
 
     path = find_path(matrix, m, n)
@@ -77,10 +86,38 @@ def alignment(x,y):
     print(new_s2)
     
 if __name__=='__main__':
+    parser.add_argument("-s1", "--sequence1_file", help="File with sequence 1", required=True)
+    parser.add_argument("-s2", "--sequence2_file", help="File with sequence 2", required=True)
+    parser.add_argument("-m", "--match", help="Match Parameter",required=True)
+    parser.add_argument("-mm", "--mismatch", help="Mismatch Parameter")
+    parser.add_argument("-g", "--gap", help="Gap Penalty Parameter")
+
+    args = parser.parse_args()
+
+
     path = [] 
 
-    #ler de um ficheiro
-    x = 'gtataatgatg'
-    y = 'ta'
+    try: 
+        s1 = open(args.sequence1_file)
+        s2 = open(args.sequence2_file)
+    except FileNotFoundError:
+        logging.error("File not found.")
+        sys.exit(1)
+    x, y = open(args.sequence1_file).read().strip(), open(args.sequence2_file).read().strip()
 
-    alignment(x,y)
+    
+    try:
+        float(args.match)
+        float(args.mismatch)
+        float(args.gap)
+        if(float(args.gap)>0):
+            logging.error("Gap must be less than or equal to 0")
+            sys.exit(1)
+    except ValueError as e:
+        logging.warning("Usage: python dynamic_needleman_wunsch.py -s1 <file1> -s2 <file2> -m <match> -mm <mismatch> -g <gap_penalty>")
+        logging.error("Match, mismatch and gap_penalty must be numbers.")
+        sys.exit(1)
+    
+    match, mismatch, gap = float(args.match), float(args.mismatch), float(args.gap)
+
+    alignment(x, y, match, mismatch, gap)
